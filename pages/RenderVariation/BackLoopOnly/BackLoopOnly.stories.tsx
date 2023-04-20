d9a23367476ef9394006e0dfccb1143cb87fd5b0
arriere-aride <arriere.aride@gmail.com>
2023-04-20 15:07:53 +0200

    feat: Add BackLoopOnly component

---
+++ pages/RenderVariation/BackLoopOnly/BackLoopOnly.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { BackLoopOnly } from "./BackLoopOnly";
+
+const meta: Meta<typeof BackLoopOnly> = {
+  title: "Crochet/RenderVariation/BackLoopOnly",
+  component: BackLoopOnly,
+  tags: ["autodocs"],
+  argTypes: {
+    fillColor: {
+      control: "color",
+    },
+  },
+  decorators: [(Story) => (<svg><Story/></svg>)]
+};
+
+export default meta;
+type Story = StoryObj<typeof BackLoopOnly>;
+
+export const Default: Story = {
+};
+export const WithDebug: Story = {
+  args: {
+    size: 24,
+    debug: true
+  }
+};
--- pages/RenderVariation/index.tsx
+++ pages/RenderVariation/index.tsx
@@ -1,1 +1,2 @@
 export {FrontLoopOnly} from "./FrontLoopOnly/FrontLoopOnly"
+export {BackLoopOnly} from "./BackLoopOnly/BackLoopOnly"
