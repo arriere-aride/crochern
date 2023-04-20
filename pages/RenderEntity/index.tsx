d0c0694f60dfdf9d462089b186d004544ee98519
arriere-aride <arriere.aride@gmail.com>
2023-04-20 08:54:19 +0200

    refactor: Add RenderEntity index file

---
+++ pages/RenderEntity/index.tsx
@@ -0,0 +1,4 @@
+export { Chain } from "./Chain/Chain";
+export { SingleCrochet } from "./SingleCrochet/SingleCrochet";
+export { SlipSnitch } from "./SlipSnitch/SlipSnitch";
+export {HalfDoubleCrochet} from "./HalfDoubleCrochet/HalfDoubleCrochet"
