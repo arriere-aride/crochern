7a0d6e1b026a8b954359b7f8feff7e4c3d7726cc
arriere-aride <arriere.aride@gmail.com>
2023-04-22 09:05:24 +0200

    feat: Add RepeatDoubleCrochetTogether component

---
+++ pages/RenderDecrease/RepeatDoubleCrochetTogether/RepeatDoubleCrochetTogether.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { RepeatDoubleCrochetTogether } from "./RepeatDoubleCrochetTogether";
+
+const meta: Meta<typeof RepeatDoubleCrochetTogether> = {
+  title: "Crochet/RenderDecrease/RepeatDoubleCrochetTogether",
+  component: RepeatDoubleCrochetTogether,
+  tags: ["autodocs"],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <svg>
+        <Story />
+      </svg>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof RepeatDoubleCrochetTogether>;
+
+export const Default: Story = {};
+export const WithFour: Story = { args: { repeat: 4 } };
+export const WithFive: Story = { args: { repeat: 5 } };
+export const WithNine: Story = { args: { repeat: 9 } };
--- pages/RenderDecrease/index.tsx
+++ pages/RenderDecrease/index.tsx
@@ -1,3 +1,4 @@
 export {TwoSingleCrochetTogether} from "./TwoSingleCrochetTogether/TwoSingleCrochetTogether"
 export {ThreeSingleCrochetTogether} from "./ThreeSingleCrochetTogether/ThreeSingleCrochetTogether"
 export {TwoDoubleCrochetTogether} from "./TwoDoubleCrochetTogether/TwoDoubleCrochetTogether"
+export {RepeatDoubleCrochetTogether} from "./RepeatDoubleCrochetTogether/RepeatDoubleCrochetTogether"
