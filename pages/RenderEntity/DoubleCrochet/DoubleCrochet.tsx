e252ee5f80e9721428d50ece754c5345179b36c6
arriere-aride <arriere.aride@gmail.com>
2023-04-24 09:35:41 +0200

    fix: Update DoubleCrochet.tsx to be pixel perfect

--- pages/RenderEntity/DoubleCrochet/DoubleCrochet.tsx
+++ pages/RenderEntity/DoubleCrochet/DoubleCrochet.tsx
@@ -2,66 +2,77 @@ import React from "react";
 
 interface DoubleCrochet {
   /**
-   * What position it should have, centered point
+   * What position it have
    */
   position?: { x: number; y: number };
   /**
    * Line size
    */
-  size: number;
+  size?: number;
   /**
    * What fill color to use
    */
   fillColor?: string;
-    /**
-   * Rotating bar, in degrees
+  /**
+   * What angle to set
    */
-    rotation: number;
+  rotation?: number;
+  /**
+   * What padding to set
+   */
+  padding?:number;
 }
 
 /** Render Half double crochet (demi-bride) */
 export const DoubleCrochet = ({
-  position = { x: 50, y: 50 },
+  position = { x: 0, y: 0 },
   size = 16,
   fillColor = "#1a1a1a",
   rotation = 0,
+  padding = 1,
   ...props
 }: DoubleCrochet) => {
   const id: string = Math.floor(Math.random() * 100).toString();
   const { x, y } = position;
   const halfSize = Math.floor(size / 2);
-  const quarterSize = Math.floor(size / 4);
+  const fifthSize = Math.floor(size / 5);
   const rotate = `rotate(${rotation})`;
+  const center = { x: x + halfSize, y: y + halfSize };
+  const box = {
+    min: { x: x + padding, y: y + padding },
+    max: { x: center.x + halfSize - padding, y: center.y + halfSize - padding },
+  };
 
   return (
     <g id={`render-item-dc--${id}`} stroke={fillColor} strokeWidth={2}>
       <line
         stroke={fillColor}
         strokeWidth={2}
-        x1={x}
-        y1={y + halfSize}
-        x2={x}
-        y2={y - halfSize}
+        x1={center.x}
+        y1={box.max.y}
+        x2={center.x}
+        y2={box.min.y}
       />
       <line
         stroke={fillColor}
         strokeWidth={2}
-        x1={x - halfSize}
-        y1={y - halfSize}
-        x2={x + halfSize}
-        y2={y - halfSize}
+        x1={box.min.x}
+        y1={box.min.y}
+        x2={box.max.x}
+        y2={box.min.y}
       />
-      <g id={`render-item-dc-bar--${id}`} 
-            transform={rotate}
-            style={{ transformOrigin: "center", transformBox: "fill-box" }}
+      <g
+        id={`render-item-dc-bar--${id}`}
+        transform={rotate}
+        style={{ transformOrigin: "center", transformBox: "fill-box" }}
       >
         <line
           stroke={fillColor}
           strokeWidth={2}
-          x1={x - quarterSize}
-          x2={x + quarterSize}
-          y1={y}
-          y2={y}
+          x1={center.x - fifthSize}
+          x2={center.x + fifthSize}
+          y1={center.y}
+          y2={center.y}
         />
       </g>
     </g>
