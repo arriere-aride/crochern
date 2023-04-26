2a3f2d652fd78c723a68cfb6a4b2df6b849f3069
arriere-aride <arriere.aride@gmail.com>
2023-04-26 19:06:58 +0200

    build: Update Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,13 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import  TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-
-import path from "path"
-
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import path from "path";
 const config: StorybookConfig = {
-  stories: [
-    "../pages/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-  ],
+  stories: ["../@(pages|components)/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
   ],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
