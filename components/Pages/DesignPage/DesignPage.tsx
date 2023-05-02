833a5eb8120244ed3eb1d9f8f1a3c6d2ed1e0b0d
arriere-aride <arriere.aride@gmail.com>
2023-05-02 11:42:02 +0200

    chore: Clean code

--- components/Pages/DesignPage/DesignPage.tsx
+++ components/Pages/DesignPage/DesignPage.tsx
@@ -35,7 +35,6 @@ export const DesignPage = ({
   gridProps,
   entityControlBarProps,
 }: DesignPage) => {
-  store.subscribe(() => console.log(store.getState()));
   return (
     <Box
       sx={{
