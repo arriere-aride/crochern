5feb7a476e2a27e2726f0e10ebd17c89cdfc7cf9
arriere-aride <arriere.aride@gmail.com>
2023-05-04 10:33:48 +0200

    chore: Clean code

--- components/Events/OnScrollResize/OnScrollResize.stories.tsx
+++ components/Events/OnScrollResize/OnScrollResize.stories.tsx
@@ -2,8 +2,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { OnScrollResize } from "./OnScrollResize";
 import { DoubleCrochet } from "@/components/RenderEntity";
-import { within } from "@storybook/testing-library";
-import { Grid } from "@/components/Grid/Grid";
 
 const meta: Meta<typeof OnScrollResize> = {
   title: "Crochet/Atoms/Events/OnScrollResize",
