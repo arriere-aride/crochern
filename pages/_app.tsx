95c55616bd8624d128a8e5062a11fb7cf2fe353d
arriere-aride <arriere.aride@gmail.com>
2023-05-06 09:45:17 +0200

    chore: Clean code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import store from "@/stores/EntityMoveStore";
+import store from "@/stores";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
