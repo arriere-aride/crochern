4e73bedf6058cd259ca569bd167986253277a8a6
arriere-aride <arriere.aride@gmail.com>
2023-05-11 11:42:47 +0200

    feat: Add yarn colors schema and first seeds

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import yarnColorsSchema from "@/schemas/schema_yarn_color.json";
+import yarnColors from "@/seeds/yarn_colors.seed.json";
+import Ajv from "ajv/dist/2019";
+import { JTDDataType } from "ajv/dist/jtd";
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
 
-type Data = {
-  name: string
-}
+type Data = JTDDataType<
+  typeof yarnColorsSchema | { errors: any }
+>;
+const ajv = new Ajv({
+  strictTuples: false,
+  allowUnionTypes: true,
+});
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const validate = ajv.compile<Data>(
+    yarnColorsSchema
+  );
+  if (!validate(yarnColors)) {
+    const { errors } = validate;
+    return res.status(200).json({ errors });
+  }
+
+  res.status(200).json(yarnColors);
 }
